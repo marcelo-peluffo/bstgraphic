39019c36a39e94d1adb1910f8b21d9e1ec8913dc
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2026-01-08 08:28:55 -0600

    created b-node class

--- bstgraphic/src/main/java/com/marcelo/BinaryNode.java
+++ bstgraphic/src/main/java/com/marcelo/BinaryNode.java
@@ -1,5 +1,21 @@
 package com.marcelo;
 
+// class implementation of binary node
 public class BinaryNode {
+    private BinaryNode left, right;
+    private Comparable<?> value;
+
+    protected BinaryNode(Comparable v) {
+        left = null;
+        right = null;
+        value = v;
+    }
     
+    public BinaryNode left() {return left;}
+    public BinaryNode right() {return right;}
+    public Comparable<?> value() {return value;}
+
+    public void setLeftTo(BinaryNode newLeftNode) {left = newLeftNode;}
+    public void setRightTo(BinaryNode newRightNode) {right = newRightNode;}
+    public void setValueTo(Comparable<?> newValue) {value = newValue;}
 }
