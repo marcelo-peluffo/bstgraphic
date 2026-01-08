740f3f3a130404e1340179df35dbc349e78f95eb
PeluffoMarceloK1714016 <K1714016@students.katyisd.org>
2026-01-08 08:34:30 -0600

    worked up to addValue() in BST class

--- bstgraphic/src/main/java/com/marcelo/BinaryTree.java
+++ bstgraphic/src/main/java/com/marcelo/BinaryTree.java
@@ -1,5 +1,19 @@
 package com.marcelo;
 
 public class BinaryTree {
+    private BinaryNode rootNode;
+
+    protected BinaryTree() {
+        rootNode = null;
+    }
     
+    protected BinaryTree(BinaryNode newRootNode) {
+        rootNode = newRootNode;
+    }
+
+    public boolean addValue(Comparable<?> newValue) {
+        
+
+        return false;
+    }
 }
