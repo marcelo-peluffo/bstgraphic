d30bd5b3181a10e55657e6a944f595e8e7a9df20
PeluffoMarceloK1714016 <k1714016@students.katyisd.org>
2026-01-24 15:21:27 -0800

    Runner class complete

--- src/main/java/com/marcelo/core/GraphicBSTRunner.java
+++ src/main/java/com/marcelo/core/GraphicBSTRunner.java
@@ -1,4 +1,11 @@
 package com.marcelo.core;
 
+/**
+ * The runner file for the Graphic BST program for CSIII.
+ * @author Marcelo
+ */
 public class GraphicBSTRunner {
+    public static void main(String[] args) {
+        GraphicBSTFrame programFrame = new GraphicBSTFrame();
+    }
 }
