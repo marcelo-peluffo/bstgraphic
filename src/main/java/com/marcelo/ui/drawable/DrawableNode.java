6fe3e980bf6efaafaa7b8afb8708e216c99032ac
PeluffoMarceloK1714016 <k1714016@students.katyisd.org>
2026-02-01 12:06:34 -0800

    drawable node interface implemetnation

--- src/main/java/com/marcelo/ui/drawable/DrawableNode.java
+++ src/main/java/com/marcelo/ui/drawable/DrawableNode.java
@@ -1,4 +1,26 @@
 package com.marcelo.ui.drawable;
 
-public interface DrawableNode {
+import com.marcelo.backend.generic.Node;
+import java.awt.Graphics;
+
+/**
+ * An interface for drawable nodes that can be painted on a Swing component
+ * and hold a reference to the backend node.
+ * 
+ * @param <T> the type of data stored in the node, which must be comparable.
+ */
+public interface DrawableNode<T extends Comparable<T>> {
+        /**
+         * Paints the node on the given graphics context.
+         * 
+         * @param g the graphics context to paint on.
+         */
+        void paint(Graphics g);
+
+        /**
+         * Returns the backend node that this drawable node wraps.
+         * 
+         * @return the backend node.
+         */
+        Node<T> getNode();
 }
