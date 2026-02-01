acfd996fcb69f74a31dde9685a8d0e9e130fa925
PeluffoMarceloK1714016 <k1714016@students.katyisd.org>
2026-02-01 12:07:39 -0800

    drawable tree interface implemetnation

--- src/main/java/com/marcelo/ui/drawable/DrawableTree.java
+++ src/main/java/com/marcelo/ui/drawable/DrawableTree.java
@@ -1,5 +1,26 @@
 package com.marcelo.ui.drawable;
 
-public interface DrawableTree {
-        
+import com.marcelo.backend.generic.Tree;
+import java.awt.Graphics;
+
+/**
+ * An interface for drawable trees that can be painted on a Swing component
+ * and hold a reference to the backend tree.
+ * 
+ * @param <T> the type of data stored in the tree, which must be comparable.
+ */
+public interface DrawableTree<T extends Comparable<T>> {
+        /**
+         * Paints all drawable nodes in the tree on the given graphics context.
+         * 
+         * @param g the graphics context to paint on.
+         */
+        void paintNodes(Graphics g);
+
+        /**
+         * Returns the backend tree that this drawable tree wraps.
+         * 
+         * @return the backend tree.
+         */
+        Tree<T> getTree();
 }
