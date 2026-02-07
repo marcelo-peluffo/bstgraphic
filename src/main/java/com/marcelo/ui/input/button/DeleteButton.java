12c199d0962fb7969276f41b4283a48ffcc07f94
PeluffoMarceloK1714016 <k1714016@students.katyisd.org>
2026-02-07 09:17:38 -0800

    program starts on event thread now

--- src/main/java/com/marcelo/ui/input/button/DeleteButton.java
+++ src/main/java/com/marcelo/ui/input/button/DeleteButton.java
@@ -3,7 +3,6 @@ package com.marcelo.ui.input.button;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
-import javax.swing.border.LineBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
